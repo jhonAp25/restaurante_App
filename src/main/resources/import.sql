6665b48a4edd2424323087ec6e23e2c5cce07db9
JhonAp25 <apazalaricojhon25@gmail.com>
2022-06-25 00:18:55 -0500

    test Api

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+insert into Plato ( descripcion, precio) values (  'arros con pollo', 10 );
+insert into Plato ( descripcion, precio) values (  'anticuchos', 15 );
+insert into Plato (descripcion, precio) values (   'tallarines', 20);
+insert into Plato ( descripcion, precio) values (  'aji de gallina', 20 );
+insert into Plato ( descripcion, precio) values (   'estofado', 20 );
+insert into Plato ( descripcion, precio) values (   'pollo al horno', 20 );
+
