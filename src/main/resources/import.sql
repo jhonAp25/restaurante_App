0ca802198e0368c2e0fd4735ec76611ee76dfc5e
JhonAp25 <apazalaricojhon25@gmail.com>
2022-07-19 20:45:08 -0500

    PLATOS insert II

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -21,3 +21,26 @@ insert into Mesa (nro_mesa, estado) values ( 'Mesa-15', true );
 
 
 insert into Mozo (nombre , apellido , dni, telefono, correo) values ( 'Jorge Carlos' , 'Velasques Bustillos' , '75689966', '897654321', 'Carlos_V11@gmail.com' )
+
+
+insert into Plato (nombre, descripcion, precio, image) values (  'Ceviche', 'Plato bandera del Perú, con una variedad de pescados marinos', 25 , 'https://elmundook.com/wp-content/uploads/2017/03/ceviche-mixto-peruano--1024x768.jpg');
+
+insert into Plato (nombre, descripcion, precio, image) values (  'Causa Rellena', 'Plato que tiene como base un tubérculo andino combinado con pollo y mayonesa', 13 , 'https://elmundook.com/wp-content/uploads/2017/03/Causa-Rellena-Plato-tradicional-de-Peru-en-Sidney-Australia-Restaurante-La-Hacienda-1-1024x768.jpg');
+
+insert into Plato (nombre, descripcion, precio, image) values (  'Lomo Saltado', 'Plato de carne de res cortada en troozos, acompañada de papas fritas y arroz', 18 , 'https://elmundook.com/wp-content/uploads/2017/03/Lomo-Saltado-en-restaurante-La-Hacienda-en-Sidney-Australia-Plato-Tipico-Peruano-1-1024x768.jpg');
+
+insert into Plato (nombre, descripcion, precio, image) values (  'Cau Cau', 'Plato hecho de mondongo de res cortado en trozos acompañado de papas y hojas de laurel', 18 , 'https://elmundook.com/wp-content/uploads/2017/03/Cau-Cau-peruano--1024x879.jpg');
+
+insert into Plato (nombre, descripcion, precio, image) values (  'Papa a la Huancaína', 'Es un plato preparado con papa cocida, crema de aji amarillo y se puede acompañar con lechuga, huevos cocidos y aceituna', 12 , 'https://elmundook.com/wp-content/uploads/2020/08/Como-preparar-papa-a-la-huancaina-plato-tipico-que-comer-en-Peru-1024x768.jpg');
+
+insert into Plato (nombre, descripcion, precio, image) values (  'Arroz Chaufa', 'Plato fusión y consiste en arroz frito, cebollas chinas, ajo, verduras, pollo, jengibre y todo se mezclará en un wok con salsa de soya ', 12 , 'https://elmundook.com/wp-content/uploads/2017/03/arroz-chaufa-con-mariscos-comida-tipica-de-Peru-1024x768.jpg');
+
+insert into Plato (nombre, descripcion, precio, image) values (  'Aji de Gallina', 'Delicioso plato peruano que consiste en pollo, papas, galletas, aceitunas, huevos, una mezcla de crema de aji y se sirve con arroz, ', 10 , 'https://i0.wp.com/www.exploorperu.com/wp-content/uploads/2020/08/Best-food-try-peru-exploor-aji-de-gallina.jpg?w=775&ssl=1');
+
+insert into Plato (nombre, descripcion, precio, image) values (  'Arroz con Pato', 'Este plato es famoso en todo el Perú y es muy probable que lo encuentren en todas partes, el arroz se cocina con pasta de cilantro, especias y cerveza oscura, acompañado de pechuga de pato ', 12 , 'https://i0.wp.com/www.exploorperu.com/wp-content/uploads/2020/08/Best-food-try-peru-exploor-arroz-con-pato.jpg?w=775&ssl=1');
+
+insert into Plato (nombre, descripcion, precio, image) values (  'Chupe de Camarones', 'Consiste en una mezcla de gambas, cebollas, ajo, tomates, crema, comino, frijoles y muchos otros ingredientes que se servirán con un huevo escalfado. ', 15 , 'https://i0.wp.com/www.exploorperu.com/wp-content/uploads/2020/08/Best-food-try-peru-exploor-chupe-de-camarones.jpg?w=775&ssl=1');
+
+insert into Plato (nombre, descripcion, precio, image) values (  'Rocoto Relleno', 'Rocoto Relleno es otra comida peruana muy deliciosa. Es un pimiento asado relleno de verduras y carne antes de festonear en la pastelería ', 13 , 'https://i0.wp.com/www.exploorperu.com/wp-content/uploads/2020/08/Best-food-try-peru-exploor-rocoto-relleno.jpg?w=775&ssl=1');
+
+insert into Plato (nombre, descripcion, precio, image) values (  'Papa Rellena', 'Papa Rellena es una croqueta rellena con una mezcla de carne. Se compone de carne molida, tomate, ajo, cebolla y pimiento. ', 12 , 'https://i0.wp.com/www.exploorperu.com/wp-content/uploads/2020/08/Best-food-try-peru-exploor-papa-rellena.jpg?w=775&ssl=1');
