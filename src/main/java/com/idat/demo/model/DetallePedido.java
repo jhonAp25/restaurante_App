56d7144c519c621c46fbbfe6480c4e70dcd7b92b
JhonAp25 <apazalaricojhon25@gmail.com>
2022-07-21 18:57:17 -0500

    bd_postgre_sql

--- src/main/java/com/idat/demo/model/DetallePedido.java
+++ src/main/java/com/idat/demo/model/DetallePedido.java
@@ -6,16 +6,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
+@Table(name = "detalle_pedido")
 public class DetallePedido {
 
     @Id
